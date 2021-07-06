f4b6606459d4bb13a95f9022650abef653b521c4
Pillars Zhang <zhangkaihao2000@qq.com>
2021-07-06 13:59:03 +0800

    防止在clone的document中二次载入脚本

--- dev/[local-develop-api] Rain Classroom PDF Direct Download.user.js
+++ dev/[local-develop-api] Rain Classroom PDF Direct Download.user.js
@@ -2,7 +2,7 @@
 // @name         [local-develop-api] Rain Classroom PDF Direct Download
 // @name:zh-CN   [本地开发接口] 雨课堂课件PDF下载工具
 // @namespace    https://www.pizyds.com/
-// @version      1.0.1
+// @version      1.0.3
 // @description  [local-develop-api] Automatic generation of direct download PDF on Rain Classroom
 // @description:zh-CN [本地开发接口] 在雨课堂页面自动生成PDF版本课件提供下载
 // @author       PillarsZhang
@@ -14,12 +14,26 @@
 // ==/UserScript==
 
 (function() {
-  'use strict';
-  console.log('[本地开发接口] 雨课堂课件PDF下载工具：已载入');
-  var scriptUrl = "https://localhost:8081/Rain%20Classroom%20PDF%20Direct%20Download.user.js";
-  fetch(scriptUrl)
-    .then(response => response.text())
-    .then(text => eval(text)) //eslint-disable-line no-eval
-    .catch(err => console.error(err));
-  //输入 npm test 以进行本地测试
+    'use strict';
+    function checkFlagMeta(){
+        return document.head.querySelector("[name~=pizyds_rain]");
+    }
+    if(!checkFlagMeta()){
+        console.log('[本地开发接口] 雨课堂课件PDF下载工具：已载入');
+        var scriptUrl = "https://localhost:8081/Rain%20Classroom%20PDF%20Direct%20Download.user.js";
+        var methods = [
+            (scriptUrl) => fetch(scriptUrl)
+              .then(response => response.text())
+              .then(text => eval(text)) //eslint-disable-line no-eval
+              .catch(err => console.error(err)),
+            (scriptUrl) => {
+                var el_script = document.createElement("script"); 
+                el_script.type = "text/javascript";
+                el_script.src = scriptUrl;
+                document.head.appendChild(el_script); 
+            }
+        ]
+        methods[1](scriptUrl);
+        //输入 npm test 以进行本地测试
+    }
 })()
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import { url_match } from "./libs/common.js";
 import add_button_download from "./libs/add_button_download.js"
 
-console.log("雨课堂课件PDF下载工具：已载入");
+if (!checkFlagMeta()){
+    addFlagMeta();
+    console.log("雨课堂课件PDF下载工具：已载入");
+    //实时查找PPT窗口
+    setInterval(()=>{
+        var url_type, el_dialog;
+        (url_type = check_url()) && (el_dialog = find_basePPTDialog()) && add_button_download(el_dialog, url_type);
+    }, 200);
+}
 
-//实时查找PPT窗口
-setInterval(()=>{
-    var url_type, el_dialog;
-    (url_type = check_url()) && (el_dialog = find_basePPTDialog()) && add_button_download(el_dialog, url_type);
-}, 200);
+function addFlagMeta(){
+    var flagMeta = document.createElement('meta');
+    flagMeta.name = 'pizyds_rain';
+    flagMeta.content = 'https://www.pizyds.com/rain-classroom-pdf-direct-download';
+    document.head.appendChild(flagMeta);
+}
+
+function checkFlagMeta(){
+    return document.head.querySelector("[name~=pizyds_rain]");
+}
 
 //更改为内部校验链接，因为大量ajax页面跳转的存在
 function check_url(){
